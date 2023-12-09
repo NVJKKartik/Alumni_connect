450090ad22dc3343575393257cafacda292cffd0
NVJKKartik <114015823+NVJKKartik@users.noreply.github.com>
2023-12-09 22:51:51 +0530

    Removed gigs

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -89,29 +89,29 @@ class _HomeScreenState extends State<HomeScreen> {
       body: ListView(
         primary: true,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 8),
-            child: SizedBox(
-              height: 400,
-              child: PageView.builder(
-                padEnds: false,
-                controller: pageController,
-                itemCount: gigs.length,
-                physics: const BouncingScrollPhysics(),
-                itemBuilder: (context, index) {
-                  bool active = index == currentPage;
-                  return GigsCard(
-                    active: active,
-                    index: index,
-                    gigs: gigs[index],
-                  );
-                },
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
+          // Padding(
+          //   padding: const EdgeInsets.only(top: 8),
+          //   child: SizedBox(
+          //     height: 400,
+          //     child: PageView.builder(
+          //       padEnds: false,
+          //       controller: pageController,
+          //       itemCount: gigs.length,
+          //       physics: const BouncingScrollPhysics(),
+          //       itemBuilder: (context, index) {
+          //         bool active = index == currentPage;
+          //         return GigsCard(
+          //           active: active,
+          //           index: index,
+          //           gigs: gigs[index],
+          //         );
+          //       },
+          //     ),
+          //   ),
+          // ),
+          // const SizedBox(
+          //   height: 20,
+          // ),
 
           //
           StreamBuilder(
@@ -192,142 +192,142 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class GigsCard extends StatelessWidget {
-  const GigsCard({
-    Key? key,
-    this.active,
-    this.index,
-    this.gigs,
-  }) : super(key: key);
+// class GigsCard extends StatelessWidget {
+//   const GigsCard({
+//     Key? key,
+//     this.active,
+//     this.index,
+//     this.gigs,
+//   }) : super(key: key);
 
-  final bool? active;
-  final int? index;
-  final Gigs? gigs;
+//   final bool? active;
+//   final int? index;
+//   final Gigs? gigs;
 
-  @override
-  Widget build(BuildContext context) {
-    final double blur = active! ? 16 : 0;
-    final double offset = active! ? 4 : 0;
-    final double top = active! ? 0 : 39;
+//   @override
+//   Widget build(BuildContext context) {
+//     final double blur = active! ? 16 : 0;
+//     final double offset = active! ? 4 : 0;
+//     final double top = active! ? 0 : 39;
 
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => GigsPage(
-              selectedTab: gigs!.currentIndex,
-            ),
-          ),
-        );
-      },
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 500),
-        curve: Curves.easeOutQuint,
-        margin: EdgeInsets.only(
-          top: top,
-          bottom: 0,
-          right: 15.5,
-          left: active! ? 16.0 : 0,
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(32),
-          color: gigs!.startColor!,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.white.withOpacity(0.1),
-              blurRadius: blur,
-              offset: Offset(0, offset),
-            ),
-          ],
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage('assets/images/${gigs!.gigImage}'),
-          ),
-        ),
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(32),
-                gradient: LinearGradient(
-                  colors: [
-                    gigs!.startColor!,
-                    gigs!.endColor!.withOpacity(0.3),
-                  ],
-                  begin: Alignment.bottomRight,
-                  end: Alignment.topLeft,
-                  tileMode: TileMode.clamp,
-                  stops: const [0.3, 0.6],
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                padding: const EdgeInsets.only(
-                  left: 24,
-                  right: 16,
-                  top: 10,
-                ),
-                height: 87,
-                decoration: BoxDecoration(
-                    color: gigs!.startColor,
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(32),
-                      bottomRight: Radius.circular(32),
-                    )),
-                child: Text(
-                  gigs!.gigInfo,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 84.75,
-              left: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 8.5,
-                        vertical: 5,
-                      ),
-                      height: 24,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.white,
-                      ),
-                      child: Text(
-                        gigs!.category,
-                        style: TextStyle(
-                          fontSize: 13,
-                          color: gigs!.startColor,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//     return InkWell(
+//       onTap: () {
+//         Navigator.push(
+//           context,
+//           MaterialPageRoute(
+//             builder: (context) => GigsPage(
+//               selectedTab: gigs!.currentIndex,
+//             ),
+//           ),
+//         );
+//       },
+//       child: AnimatedContainer(
+//         duration: const Duration(milliseconds: 500),
+//         curve: Curves.easeOutQuint,
+//         margin: EdgeInsets.only(
+//           top: top,
+//           bottom: 0,
+//           right: 15.5,
+//           left: active! ? 16.0 : 0,
+//         ),
+//         decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(32),
+//           color: gigs!.startColor!,
+//           boxShadow: [
+//             BoxShadow(
+//               color: Colors.white.withOpacity(0.1),
+//               blurRadius: blur,
+//               offset: Offset(0, offset),
+//             ),
+//           ],
+//           image: DecorationImage(
+//             fit: BoxFit.cover,
+//             image: AssetImage('assets/images/${gigs!.gigImage}'),
+//           ),
+//         ),
+//         child: Stack(
+//           children: [
+//             Container(
+//               decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.circular(32),
+//                 gradient: LinearGradient(
+//                   colors: [
+//                     gigs!.startColor!,
+//                     gigs!.endColor!.withOpacity(0.3),
+//                   ],
+//                   begin: Alignment.bottomRight,
+//                   end: Alignment.topLeft,
+//                   tileMode: TileMode.clamp,
+//                   stops: const [0.3, 0.6],
+//                 ),
+//               ),
+//             ),
+//             Positioned(
+//               bottom: 0,
+//               left: 0,
+//               right: 0,
+//               child: Container(
+//                 padding: const EdgeInsets.only(
+//                   left: 24,
+//                   right: 16,
+//                   top: 10,
+//                 ),
+//                 height: 87,
+//                 decoration: BoxDecoration(
+//                     color: gigs!.startColor,
+//                     borderRadius: const BorderRadius.only(
+//                       bottomLeft: Radius.circular(32),
+//                       bottomRight: Radius.circular(32),
+//                     )),
+//                 child: Text(
+//                   gigs!.gigInfo,
+//                   overflow: TextOverflow.ellipsis,
+//                   maxLines: 2,
+//                   style: const TextStyle(
+//                     fontSize: 18,
+//                     fontWeight: FontWeight.w600,
+//                     color: Colors.white,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             Positioned(
+//               bottom: 84.75,
+//               left: 0,
+//               right: 0,
+//               child: Padding(
+//                 padding: const EdgeInsets.symmetric(horizontal: 16),
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     Container(
+//                       padding: const EdgeInsets.symmetric(
+//                         horizontal: 8.5,
+//                         vertical: 5,
+//                       ),
+//                       height: 24,
+//                       decoration: BoxDecoration(
+//                         borderRadius: BorderRadius.circular(12),
+//                         color: Colors.white,
+//                       ),
+//                       child: Text(
+//                         gigs!.category,
+//                         style: TextStyle(
+//                           fontSize: 13,
+//                           color: gigs!.startColor,
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class PostCard extends StatelessWidget {
   final String userId;
